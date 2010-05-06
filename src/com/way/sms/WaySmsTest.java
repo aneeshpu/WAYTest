622db7d9ca8ba600d216249f1e2e6ac063ca7fd6
Aneesh P U <aneeshpu@gmail.com>
2010-05-06 20:09:05 +0530

    removed duplication in tests, replaced regular asserts with hamcrest matchers

--- src/com/way/sms/WaySmsTest.java
+++ src/com/way/sms/WaySmsTest.java
@@ -20,107 +20,86 @@ public class WaySmsTest {
 	@Test
 	public void identifies_a_way_request() throws Exception {
 
-		final SmsMessage smsMessage = createMock(SmsMessage.class);
-		expect(smsMessage.getMessageBody()).andReturn("Where are you");
-		replay(smsMessage);
-		final WaySms waySms = new WaySms(smsMessage);
-
-		assertTrue(waySms.isWayRequest());
+		final SmsMessage smsMessage = mockMessage("Where are you");
+		assertThat(new WaySms(smsMessage).isWayRequest(),is(true));
 		verify(smsMessage);
+	}
+
+	private SmsMessage mockMessage(final String messageBody) {
+		final SmsMessage smsMessage = createMock(SmsMessage.class);
+		expect(smsMessage.getMessageBody()).andReturn(messageBody);
+		replay(smsMessage);
+		return smsMessage;
 	}
 
 	@Test
 	public void identifies_a_where_are_you_request() throws Exception {
 
-		final SmsMessage smsMessage = createMock(SmsMessage.class);
-		expect(smsMessage.getMessageBody()).andReturn("where are you");
-		replay(smsMessage);
-		final WaySms waySms = new WaySms(smsMessage);
-
-		assertTrue(waySms.isWayRequest());
+		final SmsMessage smsMessage = mockMessage("where are you");
+		assertTrue(new WaySms(smsMessage).isWayRequest());
 		verify(smsMessage);
 	}
 
 	@Test
 	public void responds_to_where_r_u() throws Exception {
-		final SmsMessage smsMessage = createMock(SmsMessage.class);
-		expect(smsMessage.getMessageBody()).andReturn("Where r u");
-		replay(smsMessage);
-		final WaySms waySms = new WaySms(smsMessage);
-
-		assertTrue(waySms.isWayRequest());
+		final SmsMessage smsMessage = mockMessage("Where r u");
+		assertTrue(new WaySms(smsMessage).isWayRequest());
 		verify(smsMessage);
 
+	}
+	
+	@Test
+	public void responds_to_where_are_you_with_question_mark() throws Exception {
+		final SmsMessage smsMessage = mockMessage("Where r u?");
+		assertThat(new WaySms(smsMessage).isWayRequest(),is(true));
+		verify(smsMessage);
+		
 	}
 
 	@Test
 	public void responds_to_way() throws Exception {
-		final SmsMessage smsMessage = createMock(SmsMessage.class);
-		expect(smsMessage.getMessageBody()).andReturn("WAY");
-		replay(smsMessage);
-		final WaySms waySms = new WaySms(smsMessage);
-
-		assertThat("Should have responded to 'way'", waySms.isWayRequest(), is(true));
+		final SmsMessage smsMessage = mockMessage("WAY");
+		assertThat("Should have responded to 'way'", new WaySms(smsMessage).isWayRequest(), is(true));
 		verify(smsMessage);
 
 	}
 
 	@Test
 	public void does_not_respond_to_w() throws Exception {
-		final SmsMessage smsMessage = createMock(SmsMessage.class);
-		expect(smsMessage.getMessageBody()).andReturn("W");
-		replay(smsMessage);
-		final WaySms waySms = new WaySms(smsMessage);
-
-		assertThat("Should not have replied to just W",waySms.isWayRequest(), is(false) );
+		final SmsMessage smsMessage = mockMessage("W");
+		assertThat("Should not have replied to just W",new WaySms(smsMessage).isWayRequest(), is(false) );
 		verify(smsMessage);
 
 	}
 
 	@Test
 	public void ignores_white_spaces_in_the_request() throws Exception {
-		final SmsMessage smsMessage = createMock(SmsMessage.class);
-		expect(smsMessage.getMessageBody()).andReturn("Where            are  you ");
-		replay(smsMessage);
-		final WaySms waySms = new WaySms(smsMessage);
-
-		assertTrue(waySms.isWayRequest());
+		final SmsMessage smsMessage = mockMessage("Where            are  you ");
+		assertTrue(new WaySms(smsMessage).isWayRequest());
 		verify(smsMessage);
 
 	}
 
 	@Test
 	public void question_marks_are_allowed_at_the_end() throws Exception {
-		final SmsMessage smsMessage = createMock(SmsMessage.class);
-		expect(smsMessage.getMessageBody()).andReturn("Where            are  you?");
-		replay(smsMessage);
-		final WaySms waySms = new WaySms(smsMessage);
-
-		assertTrue(waySms.isWayRequest());
+		final SmsMessage smsMessage = mockMessage("Where            are  you?");
+		assertTrue(new WaySms(smsMessage).isWayRequest());
 		verify(smsMessage);
 
 	}
 
 	@Test
 	public void more_than_one_question_marks_are_allowed_in_the_end() throws Exception {
-		final SmsMessage smsMessage = createMock(SmsMessage.class);
-		expect(smsMessage.getMessageBody()).andReturn("Where            are  you   ????");
-		replay(smsMessage);
-		final WaySms waySms = new WaySms(smsMessage);
-
-		assertTrue(waySms.isWayRequest());
+		final SmsMessage smsMessage = mockMessage("Where            are  you   ????");
+		assertTrue(new WaySms(smsMessage).isWayRequest());
 		verify(smsMessage);
 
 	}
 
 	@Test
 	public void exclamation_marks_are_allowed_at_the_end() throws Exception {
-		final SmsMessage smsMessage = createMock(SmsMessage.class);
-		expect(smsMessage.getMessageBody()).andReturn("Where            are  you!");
-		replay(smsMessage);
-		final WaySms waySms = new WaySms(smsMessage);
-
-		assertTrue(waySms.isWayRequest());
+		final SmsMessage smsMessage = mockMessage("Where            are  you!");
+		assertTrue(new WaySms(smsMessage).isWayRequest());
 		verify(smsMessage);
 
 	}
@@ -128,12 +107,8 @@ public class WaySmsTest {
 	@Test
 	public void identifies_a_non_way_request() throws Exception {
 
-		final SmsMessage smsMessage = EasyMock.createMock(SmsMessage.class);
-		expect(smsMessage.getMessageBody()).andReturn("Where are your brains");
-		replay(smsMessage);
-		final WaySms waySms = new WaySms(smsMessage);
-
-		assertFalse(waySms.isWayRequest());
+		final SmsMessage smsMessage = mockMessage("Where are your brains");
+		assertFalse(new WaySms(smsMessage).isWayRequest());
 		verify(smsMessage);
 
 	}
