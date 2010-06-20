d6364c27cac5d0f3b0ff7174e3a355dca6944d20
Aneesh P U <aneeshpu@gmail.com>
2010-06-20 11:23:28 +0530

    removed chain handling from RegularTextMessage

--- src/com/way/sms/RegularTextMessageTest.java
+++ src/com/way/sms/RegularTextMessageTest.java
@@ -23,7 +23,7 @@ public class RegularTextMessageTest {
 	public void identifies_a_way_request() throws Exception {
 
 		final SmsMessage smsMessage = mockMessage("Where are you");
-		assertThat(new RegularTextMessage(smsMessage, EasyMock.createMock(Locator.class), new WayRequest(), EasyMock.createMock(TextMessage.class)).isWayRequest(), is(true));
+		assertThat(new RegularTextMessage(smsMessage, EasyMock.createMock(Locator.class), new WayRequest()).isWayRequest(), is(true));
 		verify(smsMessage);
 	}
 
@@ -38,14 +38,14 @@ public class RegularTextMessageTest {
 	public void identifies_a_where_are_you_request() throws Exception {
 
 		final SmsMessage smsMessage = mockMessage("where are you");
-		assertTrue(new RegularTextMessage(smsMessage, EasyMock.createMock(Locator.class), new WayRequest(), EasyMock.createMock(TextMessage.class)).isWayRequest());
+		assertTrue(new RegularTextMessage(smsMessage, EasyMock.createMock(Locator.class), new WayRequest()).isWayRequest());
 		verify(smsMessage);
 	}
 
 	@Test
 	public void responds_to_where_r_u() throws Exception {
 		final SmsMessage smsMessage = mockMessage("Where r u");
-		assertTrue(new RegularTextMessage(smsMessage, EasyMock.createMock(Locator.class), new WayRequest(), EasyMock.createMock(TextMessage.class)).isWayRequest());
+		assertTrue(new RegularTextMessage(smsMessage, EasyMock.createMock(Locator.class), new WayRequest()).isWayRequest());
 		verify(smsMessage);
 
 	}
@@ -53,7 +53,7 @@ public class RegularTextMessageTest {
 	@Test
 	public void responds_to_where_are_you_with_question_mark() throws Exception {
 		final SmsMessage smsMessage = mockMessage("Where r u?");
-		assertThat(new RegularTextMessage(smsMessage, EasyMock.createMock(Locator.class), new WayRequest(), EasyMock.createMock(TextMessage.class)).isWayRequest(), is(true));
+		assertThat(new RegularTextMessage(smsMessage, EasyMock.createMock(Locator.class), new WayRequest()).isWayRequest(), is(true));
 		verify(smsMessage);
 
 	}
@@ -61,7 +61,7 @@ public class RegularTextMessageTest {
 	@Test
 	public void responds_to_way() throws Exception {
 		final SmsMessage smsMessage = mockMessage("WAY");
-		assertThat("Should have responded to 'way'", new RegularTextMessage(smsMessage, EasyMock.createMock(Locator.class), new WayRequest(), EasyMock.createMock(TextMessage.class)).isWayRequest(),
+		assertThat("Should have responded to 'way'", new RegularTextMessage(smsMessage, EasyMock.createMock(Locator.class), new WayRequest()).isWayRequest(),
 				is(true));
 		verify(smsMessage);
 
@@ -71,29 +71,16 @@ public class RegularTextMessageTest {
 	public void does_not_respond_to_w() throws Exception {
 		final SmsMessage smsMessage = mockMessage("W");
 
-		assertThat("Should not have replied to just W", new RegularTextMessage(smsMessage, EasyMock.createMock(Locator.class), new WayRequest(), null).isWayRequest(), is(false));
+		assertThat("Should not have replied to just W", new RegularTextMessage(smsMessage, EasyMock.createMock(Locator.class), new WayRequest()).isWayRequest(), is(false));
 		verify(smsMessage);
 
 	}
 
-	@Test
-	public void delegates_to_next_handler_if_not_its_own_request() throws Exception {
-		final SmsMessage smsMessage = mockMessage("W");
-
-		final TextMessage textMessageHandler = EasyMock.createMock(TextMessage.class);
-		expect(textMessageHandler.isWayRequest()).andReturn(true);
-		replay(textMessageHandler);
-
-		assertThat("The delgatee should have replied in the affirmative", new RegularTextMessage(smsMessage, EasyMock.createMock(Locator.class), new WayRequest(), textMessageHandler).isWayRequest(),
-				is(true));
-		verify(smsMessage);
-
-	}
 
 	@Test
 	public void ignores_white_spaces_in_the_request() throws Exception {
 		final SmsMessage smsMessage = mockMessage("Where            are  you ");
-		assertTrue(new RegularTextMessage(smsMessage, EasyMock.createMock(Locator.class), new WayRequest(), EasyMock.createMock(TextMessage.class)).isWayRequest());
+		assertTrue(new RegularTextMessage(smsMessage, EasyMock.createMock(Locator.class), new WayRequest()).isWayRequest());
 		verify(smsMessage);
 
 	}
@@ -101,7 +88,7 @@ public class RegularTextMessageTest {
 	@Test
 	public void question_marks_are_allowed_at_the_end() throws Exception {
 		final SmsMessage smsMessage = mockMessage("Where            are  you?");
-		assertTrue(new RegularTextMessage(smsMessage, EasyMock.createMock(Locator.class), new WayRequest(), EasyMock.createMock(TextMessage.class)).isWayRequest());
+		assertTrue(new RegularTextMessage(smsMessage, EasyMock.createMock(Locator.class), new WayRequest()).isWayRequest());
 		verify(smsMessage);
 
 	}
@@ -109,7 +96,7 @@ public class RegularTextMessageTest {
 	@Test
 	public void more_than_one_question_marks_are_allowed_in_the_end() throws Exception {
 		final SmsMessage smsMessage = mockMessage("Where            are  you   ????");
-		assertTrue(new RegularTextMessage(smsMessage, EasyMock.createMock(Locator.class), new WayRequest(), EasyMock.createMock(TextMessage.class)).isWayRequest());
+		assertTrue(new RegularTextMessage(smsMessage, EasyMock.createMock(Locator.class), new WayRequest()).isWayRequest());
 		verify(smsMessage);
 
 	}
@@ -117,7 +104,7 @@ public class RegularTextMessageTest {
 	@Test
 	public void exclamation_marks_are_allowed_at_the_end() throws Exception {
 		final SmsMessage smsMessage = mockMessage("Where            are  you!");
-		assertTrue(new RegularTextMessage(smsMessage, EasyMock.createMock(Locator.class), new WayRequest(), EasyMock.createMock(TextMessage.class)).isWayRequest());
+		assertTrue(new RegularTextMessage(smsMessage, EasyMock.createMock(Locator.class), new WayRequest()).isWayRequest());
 		verify(smsMessage);
 
 	}
@@ -126,7 +113,7 @@ public class RegularTextMessageTest {
 	public void identifies_a_non_way_request() throws Exception {
 
 		final SmsMessage smsMessage = mockMessage("Where are your brains");
-		assertFalse(new RegularTextMessage(smsMessage, EasyMock.createMock(Locator.class), new WayRequest(), EasyMock.createMock(TextMessage.class)).isWayRequest());
+		assertFalse(new RegularTextMessage(smsMessage, EasyMock.createMock(Locator.class), new WayRequest()).isWayRequest());
 		verify(smsMessage);
 
 	}
@@ -139,7 +126,7 @@ public class RegularTextMessageTest {
 
 		replay(smsMessage);
 
-		final RegularTextMessage waySms = new RegularTextMessage(smsMessage, EasyMock.createMock(Locator.class), new WayRequest(), EasyMock.createMock(TextMessage.class));
+		final RegularTextMessage waySms = new RegularTextMessage(smsMessage, EasyMock.createMock(Locator.class), new WayRequest());
 		assertEquals(fromAddress, waySms.from());
 
 		verify(smsMessage);
@@ -150,7 +137,7 @@ public class RegularTextMessageTest {
 		final SmsMessage smsMessage = EasyMock.createMock(SmsMessage.class);
 
 		final Locator locator = mockLocator();
-		final RegularTextMessage waySms = new RegularTextMessage(smsMessage, locator, new WayRequest(), EasyMock.createMock(TextMessage.class));
+		final RegularTextMessage waySms = new RegularTextMessage(smsMessage, locator, new WayRequest());
 
 		final String reply = waySms.generateReply();
 
@@ -166,7 +153,7 @@ public class RegularTextMessageTest {
 		final Locator locator = mockLocator();
 		final TextMessage textMessageHandler = EasyMock.createMock(TextMessage.class);
 
-		final RegularTextMessage waySms = new RegularTextMessage(smsMessage, locator, new WayRequest(), textMessageHandler);
+		final RegularTextMessage waySms = new RegularTextMessage(smsMessage, locator, new WayRequest());
 
 		final String reply = waySms.generateReply();
 
